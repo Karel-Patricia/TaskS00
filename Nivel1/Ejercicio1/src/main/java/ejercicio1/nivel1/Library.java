cc78a1108c23ee80ae667f5db874c0249a8daed8
Patricia Paredes <patriciakarel@hotmail.com>
2026-01-16 14:11:58 +0100

    test: configure JUnit 5 and AssertJ dependencies

---
+++ Nivel1/Ejercicio1/src/main/java/ejercicio1/nivel1/Library.java
@@ -0,0 +1,18 @@
+package ejercicio1.nivel1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+
+    private List<String> books;
+
+    public Library() {
+        books = new ArrayList<>();
+    }
+
+    public List<String> getBooks() {
+        return books;
+    }
+
+}
